0240120681d92181f8f41565bee4996e68146daa
yun <benja.junemann@gmail.com>
2023-07-21 00:20:56 -0400

    git ignore

--- OCI LABS/Clases/Clase 13/Contest/B/B13.cpp
+++ OCI LABS/Clases/Clase 13/Contest/B/B13.cpp
@@ -1,2 +1,23 @@
-#include <bits/stdc++.h>
-using namespace std;
+#include <bits/stdc++.h>
+using namespace std;
+
+string reemplazar(string cadena ,string s1, string s2){
+    for()
+}
+
+
+
+
+int main(){
+    int m,n;
+    string s1,s2;
+    cin>>m>>n;
+    vector <string> cadenas(n);
+    for(int i=0;i<m;i++){
+        cin>>s1>>s2;
+        for(int j=0;j<n;j++){
+            cin>>cadenas[j];
+
+        }
+    }   
+}
