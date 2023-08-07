273978ac759373ccdd26ace65672dd9b931b76e4
yun <benja.junemann@gmail.com>
2023-08-06 21:15:25 -0400

    Hackerank

---
+++ Ejercicios/Hackerank/Data_types.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+    int a;
+    long b;
+    char c;
+    float d;
+    double e;
+    cin>>a>>b>>c>>d>>e;
+    cout<<a<<endl<<b<<endl<<c<<endl<<setprecision(3)<<d<<endl<<setprecision(9)<<e;
+    return 0;
+}
