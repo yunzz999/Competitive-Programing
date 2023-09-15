ec21f92080fe246b00d2d99202771dd84dbfb527
yun <benja.junemann@gmail.com>
2023-09-14 23:52:56 -0300

    Ejercicios

---
+++ Ejercicios/CodeForces/520A.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+   int n;
+   cin>>n;
+   int x[n];
+   bool panagram = true;
+   vector<char>sentence(n);
+   for(int i=0;i<n;i++){
+      cin>>sentence[i];
+      
+   }
+
+
+}
